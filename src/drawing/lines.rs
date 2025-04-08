420abe867e00b982aefc43c259f97607e0385a21
Mqlvin <pricklydotcactus@gmail.com>
2025-04-08 15:23:17 +0100

    add test drawing impl

---
+++ src/drawing/lines.rs
@@ -0,0 +1,55 @@
+
+use crate::drawing::{DrawMethod, DrawParameters};
+use crate::hardware::PhysicalDimensions;
+use serde::{Serialize, Deserialize};
+use crate::drawing::DrawSurface;
+
+pub struct LinesMethod;
+
+impl DrawMethod for LinesMethod {
+    type DrawParameters = LinesParameters;
+
+    fn get_id(&self) -> &'static str {
+        "lines"
+    }
+
+    fn get_formatted_name(&self) -> &'static str {
+        "Lines"
+    }
+
+    fn gen_instructions(&self, physical_dimensions: &PhysicalDimensions, parameters: &LinesParameters) -> Vec<u8> {
+        
+        let mut surface = DrawSurface::new(0., 0., physical_dimensions);
+        
+        for i in (0..parameters.num_lines) {
+            for x in (0..=100) {
+                if i % 2 == 0 {
+                    let x = (physical_dimensions.page_width() / 100.) * x as f64;
+                    surface.sample_xy(x, i as f64 * 10.);
+                } else {
+                    let x = physical_dimensions.page_width() - (physical_dimensions.page_width() / 100.) * x as f64;
+                    surface.sample_xy(x, i as f64 * 10.);
+                }
+            }
+
+            let (current_x, current_y) = surface.get_xy();
+            println!("Current xy {} {}", current_x, current_y);
+            for y in (0..10) {
+                surface.sample_xy(current_x, current_y + y as f64);
+            }
+        }
+
+        println!("Drawing {} lines", parameters.num_lines);
+
+        surface.current_ins
+    }
+}
+
+
+
+#[derive(Serialize, Deserialize)]
+pub struct LinesParameters {
+    pub num_lines: u32
+}
+
+impl DrawParameters for LinesParameters {}
