4496d72c3757e89023442e84b9269d436d46ac10
Mqlvin <pricklydotcactus@gmail.com>
2025-05-01 19:21:33 +0100

    add dead scribble lerp code

--- src/drawing/scribble.rs
+++ src/drawing/scribble.rs
@@ -4,6 +4,7 @@ use crate::hardware::PhysicalDimensions;
 use serde::{Serialize, Deserialize};
 use crate::drawing::DrawSurface;
 use crate::drawing::util::*;
+use ordered_float::OrderedFloat;
 
 ///
 /// An empty struct to implement the "Scribbles" draw method on.
@@ -59,16 +60,24 @@ impl DrawMethod for ScribbleMethod {
             let dist_to_next = ((stippled_points[t[1]].x / 5. - stippled_points[t[0]].x / 5.).powi(2) + (stippled_points[t[1]].y / 5. - stippled_points[t[0]].y / 5.).powi(2)).sqrt();
 
             let radius = dist_to_next / radius_divisor;
-            for i in 0..(parameters.scribble_size / 2) {
-                let offset_x = f32::sin((i as f32 * (std::f32::consts::PI)) / 10.) * radius;
-                let offset_y = f32::cos((i as f32 * (std::f32::consts::PI)) / 10.) * radius;
+            let iterations: usize = ((radius * 4.) as usize).max(6);
+            for i in 0..=iterations {
+                let theta = 2. * std::f32::consts::PI * (i as f32 / (iterations) as f32);
+                let offset_x = f32::sin(theta) * radius;
+                let offset_y = f32::cos(theta) * radius;
+                // let lerped = lerp_xy(scaled_x + offset_x, stippled_points[t[1]].x / 5., scaled_y + offset_y, stippled_points[t[1]].y / 5., (i as f32 / iterations as f32));
 
-                surface.sample_xy((scaled_x + offset_x).into_inner() as f64, (scaled_y + offset_y as f32).into_inner() as f64);
+                // surface.sample_xy((scaled_x + offset_x + lerped.0).into_inner() as f64, (scaled_y + offset_y + lerped.1).into_inner() as f64);
+                 surface.sample_xy((scaled_x + offset_x).into_inner() as f64, (scaled_y + offset_y + parameters.vertical_offset).into_inner() as f64);
             }
         }
 
         surface.current_ins
     }
+}
+
+fn lerp_xy(x1: OrderedFloat<f32>, x2: OrderedFloat<f32>, y1: OrderedFloat<f32>, y2: OrderedFloat<f32>, lerp: f32) -> (f32, f32) {
+    ( (x2 - x1).into_inner() * (lerp / 1.) , (y2 - y1).into_inner() * (lerp / 1.) )
 }
 
 
@@ -85,7 +94,8 @@ pub struct ScribbleParameters {
     num_stipples: usize,
     num_iterations: usize,
     relaxation_tendency: f32,
-    scribble_size: usize
+    scribble_size: usize,
+    vertical_offset: f32,
 }
 
 impl DrawParameters for ScribbleParameters {}
